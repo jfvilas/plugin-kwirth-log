066b48937928088f1218da555ff93d713c133185
Julio Fernandez <jfvilas@outlook.com>
2025-11-02 13:09:51 +0100

    add final version

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-export const VERSION='0.12.9'
+export const VERSION='0.13.2'
 
 export { EntityKwirthLogContent, kwirthLogPlugin } from './plugin'
 export { KwirthLogClient, kwirthLogApiRef  } from './api'
